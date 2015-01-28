43311ae11bfaf622bfab2aeb7b997b8e5626ffa4
Lester Crespo <lester.crespo.es@gmail.com>
2015-01-28 18:17:40 +0000

    Added numeric check functions

--- numeric_check.h
+++ numeric_check.h
@@ -9,6 +9,7 @@
 #define NUMERIC_CHECK_H_
 
 #include <numeric>
+#include <limits>
 #include "stddef.h"
 
 /*
@@ -50,13 +51,13 @@ namespace Range
      * true means static cast to A
      */
     template<size_t a,size_t b,bool intA>
-    struct _is_le_cast_to_a_
+    struct is_le_cast_to_a_
     {
         constexpr static const bool value = (a > b) ? true : (a < b) ? false : intA ? false : true;
     };
     // comparation function using cast
     template<bool>
-    struct _cast_cmp
+    struct cast_cmp_
     {
         template <class A,class B>
         static inline bool le(A a,B b);
@@ -64,14 +65,14 @@ namespace Range
     // cast to A implementation
     template<>
     template<class A,class B>
-    inline bool _cast_cmp<true>::le(A a,B b)
+    inline bool cast_cmp_<true>::le(A a,B b)
     {
         return (a <= static_cast<A>(b));
     }
     // cast to b implementation
     template<>
     template<class A,class B>
-    inline bool _cast_cmp<false>::le(A a,B b)
+    inline bool cast_cmp_<false>::le(A a,B b)
     {
         return (static_cast<B>(a) <= b);
     }
@@ -81,7 +82,7 @@ namespace Range
      * is A int, is B int
      */
     template<bool intA,bool intB>
-    struct _signed_unsigned_cmp
+    struct signed_unsigned_cmp_
     {
         template<class A,class B>
         static inline bool less_equal(A a,B b)
@@ -92,28 +93,75 @@ namespace Range
     // Specialization for uint int
     template<>
     template<class A,class B>
-    inline bool _signed_unsigned_cmp<false,true>::less_equal(A a,B b)
+    inline bool signed_unsigned_cmp_<false,true>::less_equal(A a,B b)
     {
-        return (b >=0) && _cast_cmp< _is_le_cast_to_a_<sizeof(A),sizeof(B),std::is_signed<A>::value >::value >::le(a,b);
+        return (b >=0) && cast_cmp_< is_le_cast_to_a_<sizeof(A),sizeof(B),std::is_signed<A>::value >::value >::le(a,b);
     }
     // Specialization for int uint
     template<>
     template<class A,class B>
-    inline bool _signed_unsigned_cmp<true,false>::less_equal(A a,B b)
+    inline bool signed_unsigned_cmp_<true,false>::less_equal(A a,B b)
     {
-       return (a <=0) || _cast_cmp< _is_le_cast_to_a_<sizeof(A),sizeof(B),std::is_signed<A>::value >::value >::le(a,b);
+       return (a <=0) || cast_cmp_< is_le_cast_to_a_<sizeof(A),sizeof(B),std::is_signed<A>::value >::value >::le(a,b);
     }
 
     template<class A, class B>
     static inline bool less_equal(A a, B b)
     {
-        return _signed_unsigned_cmp<std::is_signed<A>::value,std::is_signed<B>::value>::less_equal(a, b);
+        return signed_unsigned_cmp_<std::is_signed<A>::value,std::is_signed<B>::value>::less_equal(a, b);
     }
 
     template<class V, class T>
     static bool check(V v, T start, T end)
     {
         return less_equal(start, v) && less_equal(v, end);
+    }
+
+    template<class A, class B>
+    static bool double_equal(A a,B b)
+    {
+        static_assert(std::is_floating_point<A>::value && std::is_floating_point<B>::value,"Only floating point arguments are allowed");
+
+    }
+    /*
+     * Struct for number convertion
+     */
+    template<class O,class I>
+    struct safe_cast_
+    {
+        /**
+         * Convert one type to another in a safe way
+         */
+        static O get(I input)
+        {
+           constexpr O min = std::is_floating_point<O>::value ? -std::numeric_limits<O>::max() : std::numeric_limits<O>::min();
+           constexpr O max = std::numeric_limits<O>::max();
+           if (!check(input,min,max))
+               throw std::out_of_range((Faz::FormatString() << "Value " << input << " is not between " << +min << " and " << +max).str());
+           return static_cast<O>(input);
+        }
+    };
+    //specialization for the same type
+    template<class T>
+    struct safe_cast_<T,T>
+    {
+        static inline T  get(T input)
+        {
+            return input;
+        }
+    };
+
+    // Template argument deduction only works on function arguments, not on the return type
+    template<class O, class I>
+    O get_safe(I input,const O&) {        // template can not be deduce by the out type
+        return safe_cast_<O,I>::get(input);
+    }
+
+    template<class T>
+    T string_to_uint(const char* str)
+    {
+        auto t = std::stoul(str);
+        return get_safe<T>(t,T());
     }
 
     void test()
